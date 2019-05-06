bd354ad5b8e2c7924efb6c4c11a72e81410b586b
ndass <nd@uscomputers.net>
2019-05-05 19:52:46 -0700

    Add keyboard controller.

---
+++ Assets/Scripts/KeyboardController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardController : MonoBehaviour
+{
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.position = transform.position + new Vector3(-0.1f, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.position = transform.position + new Vector3(0.1f, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            transform.position = transform.position + new Vector3(0, 0, 0.1f);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.position = transform.position + new Vector3(0, 0, -0.1f);
+        }
+    }
+}
