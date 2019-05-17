df0fe436ee8edda54dd13dbaa3defb883dcaed61
ndass <nd@uscomputers.net>
2019-05-17 01:10:18 -0700

    Finish sound functionality.

--- Assets/Scripts/TextToSpeech.cs
+++ Assets/Scripts/TextToSpeech.cs
@@ -11,16 +11,8 @@ using OVR;
 public class TextToSpeech : MonoBehaviour
 {
     public TextMesh debugText;
-    public SoundFX audio;
+    public SoundFX sound;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //audio = gameObject.GetComponent<AudioSource>();
-        //StartCoroutine(PlayTranslateAudio("test"));
-        StartCoroutine(PlayCloudAudio("test"));
-    }
 
     // Update is called once per frame
     void Update()
@@ -35,41 +27,21 @@ public class TextToSpeech : MonoBehaviour
 
         if (! (www.isNetworkError || www.isHttpError))
         {
-            // Show results as text
             if (www.downloadHandler.text != "")
             {
-                //Debug.Log(www.downloadHandler.text);
-                StartCoroutine(PlayTranslateAudio(www.downloadHandler.text));
+                StartCoroutine(PlayCloudAudio(www.downloadHandler.text));
             }
         }
     }
 
-    IEnumerator PlayTranslateAudio(string text)
+    IEnumerator PlayCloudAudio(string num)
     {
-        debugText.text = text;
-        text = text.Replace(" ", "%20");
-        string url = "https://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&q=" + text + "&tl=En-US";
+        debugText.text = num;
+        string url = "http://gmdavis.pythonanywhere.com/multivrse/" + num;
         WWW www = new WWW(url);
         yield return www;
-        //Debug.Log(Application.dataPath + "/test.mp3");
-        //System.IO.File.WriteAllBytes(Application.dataPath + "/test.mp3", www.bytes);
-        //AudioClip clip = www.GetAudioClip(false, true, AudioType.MPEG);
-        //clip.loadType = DecompressOnLoad;
-        //EncodeMP3.convert(clip, Application.dataPath + "/test.mp3", 128);
-
-        //audio.soundClips[0] = clip;
-        //audio.PlaySound();
+        sound.soundClips[0] = www.GetAudioClip(false, true, AudioType.MPEG);
         debugText.text = "Finished playing";
-    }
-
-    IEnumerator PlayCloudAudio(string text)
-    {
-        debugText.text = text;
-        string url = "http://gmdavis.pythonanywhere.com/multivrse/1";
-        WWW www = new WWW(url);
-        yield return www;
-        audio.soundClips[0] = www.GetAudioClip(false, true, AudioType.MPEG);
-        debugText.text = "Finished playing";
-        audio.PlaySound();
+        sound.PlaySound();
     }
 }
