315ec35dd6890299cf8b836015909dbcd9729500
tdeeb <kimimaru@posteo.net>
2021-01-28 13:55:00 -0800

    More tune-ups
    -Added clickable goals to the garage scenario
    -Updated doctor intro and goals
    -Added notes visible on the bottom-left of both scenarios

---
+++ Assets/Scripts/CopyInputFieldText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CopyInputFieldText : MonoBehaviour
+{
+    [SerializeField] private TMPro.TMP_InputField InputFieldCopyFrom = null;
+    [SerializeField] private TextMeshProUGUI TextCopyTo = null;
+
+    private void Start()
+    {
+        InputFieldCopyFrom.onValueChanged.AddListener(OnInputValueChanged);
+    }
+
+    private void OnDestroy()
+    {
+        if (InputFieldCopyFrom != null)
+        {
+            InputFieldCopyFrom.onValueChanged.RemoveListener(OnInputValueChanged);
+        }
+    }
+
+    private void OnInputValueChanged(string newValue)
+    {
+        TextCopyTo.text = newValue;
+    }
+}
