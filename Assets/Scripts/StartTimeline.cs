f0d70865dadf7f45f87f8bb722053682641f1651
unity.cloud <unity.cloud@spatial.digital>
2021-06-08 17:15:49 -0700

    Added a transition sequence for the doctor scene

--- Assets/Scripts/StartTimeline.cs
+++ Assets/Scripts/StartTimeline.cs
@@ -7,15 +7,9 @@ using UnityEngine.Playables;
 public class StartTimeline : MonoBehaviour
 {
     [SerializeField] PlayableDirector Director = null;
-    [SerializeField] private Collider ColliderToDisable = null;
 
     public void PlayTimeline()
     {
         Director.Play();
-
-        if (ColliderToDisable != null)
-        {
-            ColliderToDisable.enabled = false;
-        }
     }
 }
