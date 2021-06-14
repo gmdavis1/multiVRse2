2b009e410b3c6c857c80a85d0c5cf7c87408418c
unity.cloud <unity.cloud@spatial.digital>
2021-06-14 15:44:00 -0700

    Added a pause feature and brightened the patient room in the doctor scene

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    [SerializeField] private AudioSource AudioSrc = null;
+    [SerializeField] private GameObject Overlay = null;
+
+    [SerializeField] private GameObject PauseBtn = null;
+    [SerializeField] private GameObject UnpauseBtn = null;
+
+    public void Pause()
+    {
+        SetPause(true);
+    }
+
+    public void Unpause()
+    {
+        SetPause(false);
+    }
+
+    private void SetPause(in bool paused)
+    {
+        Overlay.SetActive(paused);
+        AudioSrc.mute = paused;
+
+        PauseBtn.SetActive(!paused);
+        UnpauseBtn.SetActive(paused);
+    }
+}
