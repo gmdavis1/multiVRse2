bc925745510b4289fc6cadcd30063d2ff4153d4c
unity.cloud <unity.cloud@spatial.digital>
2021-06-24 16:41:44 -0700

    Opened URLs in a new tab on WebGL

--- Assets/Scripts/IntroSceneURLLinker.cs
+++ Assets/Scripts/IntroSceneURLLinker.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
 public class IntroSceneURLLinker : MonoBehaviour
 {
+    [DllImport("__Internal")]
+    private static extern void OpenURLInTab(string url);
+    
     [SerializeField] private TextMeshProUGUI URLText = null;
     [SerializeField] private Button URLButton = null;
     [SerializeField] SceneURLData[] SceneURLs = System.Array.Empty<SceneURLData>();
@@ -31,7 +35,13 @@ public class IntroSceneURLLinker : MonoBehaviour
 
     public void OnURLClicked()
     {
-        Application.OpenURL(URLText.text);
+        string link = URLText.text;
+        
+        #if !UNITY_EDITOR && UNITY_WEBGL
+            OpenURLInTab(link);
+        #else
+            Application.OpenURL(link);
+        #endif
     }
 
     [System.Serializable]
