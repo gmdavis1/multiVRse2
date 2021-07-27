cc8ede47aafbd0614cc6a2c8e440828c226de440
Glenn M. Davis <gmdavis1@gmail.com>
2021-07-26 21:18:26 -0700

    add sound check button

---
+++ Assets/Scripts/Debriefing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Debriefing : MonoBehaviour
+{
+    [DllImport("__Internal")]
+    private static extern void OpenURLInTab(string url);
+
+    public void OpenURL()
+    {
+        string link = "https://stanforduniversity.qualtrics.com/jfe/form/SV_7QXqGaXrlMFHcVg?Q_Language=ZH-S";
+        #if !UNITY_EDITOR && UNITY_WEBGL
+            OpenURLInTab(link);
+        #else
+            Application.OpenURL(link);
+        #endif
+    }
+}
